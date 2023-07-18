cd14870e31d950aa746ff341f64ce4240e749ca2
EstebanAG1005 <60299338+EstebanAG1005@users.noreply.github.com>
2023-07-18 17:37:33 -0600

    Cambios para mejoras al codigo

--- main.py
+++ main.py
@@ -1,40 +1,15 @@
 import sys
-import graphviz
 from antlr4 import *
+from antlr4.tree.Trees import Trees
 from ExprLexer import ExprLexer
 from ExprParser import ExprParser
 from antlr4.error.ErrorListener import ErrorListener
-from antlr4.tree.Trees import Trees
 
 
 class CustomErrorListener(ErrorListener):
     def syntaxError(self, recognizer, offendingSymbol, line, column, msg, e):
         # Añade tu propia lógica aquí para manejar los errores
         print(f"Línea {line}:{column} {msg}")
-
-
-class DotExport(ParseTreeListener):
-    def __init__(self):
-        self._graph = graphviz.Digraph(format="pdf")
-        self._node_id = 0
-
-    def enterEveryRule(self, ctx):
-        self._node_id += 1
-        node_text = Trees.getNodeText(ctx, ruleNames=ctx.parser.ruleNames)
-        self._graph.node(str(self._node_id), label=node_text)
-        if ctx.parentCtx is not None:
-            parent_id = ctx.parentCtx.invokingState
-            self._graph.edge(str(parent_id), str(self._node_id))
-
-    def visitTerminal(self, node):
-        self._node_id += 1
-        self._graph.node(str(self._node_id), label=node.symbol.text)
-        if node.parentCtx is not None:
-            parent_id = node.parentCtx.invokingState
-            self._graph.edge(str(parent_id), str(self._node_id))
-
-    def export(self, filename):
-        self._graph.render(filename)
 
 
 def main(argv):
@@ -53,11 +28,8 @@ def main(argv):
     tree = parser.prog()
 
     # Exportar el árbol en formato DOT
-    exporter = DotExport()
-    walker = ParseTreeWalker()
-    walker.walk(exporter, tree)
-
-    exporter.export("tree")
+    with open("tree.dot", "w") as file:
+        file.write(Trees.toStringTree(tree, None, parser))
 
 
 if __name__ == "__main__":
