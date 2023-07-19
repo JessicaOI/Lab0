0536cb7445d13c9839ce8577d64e812b31e32c32
EstebanAG1005 <60299338+EstebanAG1005@users.noreply.github.com>
2023-07-18 19:10:13 -0600

    Tests

--- main.py
+++ main.py
@@ -4,12 +4,30 @@ from antlr4.tree.Trees import Trees
 from ExprLexer import ExprLexer
 from ExprParser import ExprParser
 from antlr4.error.ErrorListener import ErrorListener
+from graphviz import Digraph
 
 
 class CustomErrorListener(ErrorListener):
     def syntaxError(self, recognizer, offendingSymbol, line, column, msg, e):
         # Añade tu propia lógica aquí para manejar los errores
         print(f"Línea {line}:{column} {msg}")
+
+
+def plot_tree(parser, tree):
+    dot = Digraph()
+
+    def plot_node(node, parent=None):
+        name = f"{node}"
+        dot.node(name, label=str(node))
+        if parent is not None:
+            dot.edge(parent, name)
+        children = Trees.getChildren(node)
+        print(f"Children of {node}: {children}")  # Imprimir los hijos para depuración
+        for child in children:
+            plot_node(child, parent=name)
+
+    plot_node(tree)  # Only pass tree, not tree.tree
+    dot.render("output.gv", view=True)
 
 
 def main(argv):
@@ -27,9 +45,11 @@ def main(argv):
 
     tree = parser.prog()
 
-    # Exportar el árbol en formato DOT
-    with open("tree.dot", "w") as file:
-        file.write(Trees.toStringTree(tree, None, parser))
+    # Imprimir la representación de texto del árbol
+    print(Trees.toStringTree(tree, None, parser))
+
+    # Dibujar el árbol de análisis
+    plot_tree(parser, tree)
 
 
 if __name__ == "__main__":
