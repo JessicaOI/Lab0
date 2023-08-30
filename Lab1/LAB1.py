b085ff237da3d8141de099244cc2dc90cebdcd51
EstebanAG1005 <60299338+EstebanAG1005@users.noreply.github.com>
2023-08-30 16:13:42 -0600

    testing

--- Lab1/LAB1.py
+++ Lab1/LAB1.py
@@ -4,6 +4,7 @@ from antlr4 import *
 from antlr4.tree.Trees import Trees
 from YAPLLexer import YAPLLexer
 from YAPLParser import YAPLParser
+from YAPLListener import YAPLListener
 from antlr4.error.ErrorListener import ErrorListener
 from anytree import Node, RenderTree
 from anytree.exporter import DotExporter
@@ -166,7 +167,7 @@ class SymbolTable:
         )
 
 
-class MyYAPLListener(ParseTreeListener):
+class MyYAPLListener(YAPLListener):
     def __init__(self):
         self.symbol_table = SymbolTable()
         self.current_scope = "global"
